29c1e3ead2ddc01382a04b265fcd9c5822a08175
achmad habibi <44155889+habibiachmad@users.noreply.github.com>
2020-04-30 15:49:36 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     player1.King = "Jalan 1 step kesetiap arah";
     player1.Queen = "Jalan Serong dan Lurus sampai Ujung Board";
     player1.Bishop = "Jalan Serong ";
+    player1.Knight = "Jalan Bentuk L";
     player1.Rook = "Jalan Lurus ";
     player1.Pion = "Jalan 1 step kedepan(Kecuali langkanh Pertama bisa 2 step)";
     Bentuk.bentuk = "Persegi";
